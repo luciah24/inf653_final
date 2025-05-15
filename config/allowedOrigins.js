4d9e29d3327afa7a9b8fe476ad3ab256348d78b8
Abriana Chenet <a_chenet@mail.fhsu.edu>
2025-05-14 23:05:43 -0500

    updated glitch url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https:/your-project-name.glitch.me',
+    'https://cooked-marred-cemetery.glitch.me',
     'https://dazzling-snickerdoodle-777101.netlify.app', // allow CORS for the automated testing app
     'http://127.0.0.1:5500', 
     'http://localhost:3500',
